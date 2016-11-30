9028629dfe161441d1dc1f97fe56560b0385b3f8
archer77 <corleyx@yahoo.com>
2016-11-30 11:47:44 -0800

    add first story "pizza parlor"

---
+++ madLibsStore.js
@@ -0,0 +1,19 @@
+//madLibsStore.js
+//Author:Alexander Corley
+
+var PizzaParlor = {
+  
+  words : {
+    1:"Name",
+    2:"Adjective",
+    3:"Noun",
+    4:"Adjective",
+    5:"Food (Plural)",
+    6:"Noun (Plural)",
+    7:"Verb Ending in \"ed\"",
+    8:"Liquid",
+    9:"Noun"
+  },
+  
+  Story : "Come on over to <span id=\"\" class=\"words\"></span>`s Pizza Parlor where you can enjoy you favorite <span id=\"\" class=\"words\"></span>-dish pizza`s. You can try our famous <span id=\"\" class=\"words\"></span>-lovers pizza, or select from our list of <span id=\"\" class=\"words\"></span> toppings, including sauteed <span id=\"\" class=\"words\"></span>, <span id=\"\" class=\"words\"></span>, and many more. Our crusts are hand-<span id=\"\" class=\"words\"></span> and basted in <span id=\"\" class=\"words\"></span> to make them seem more <span id=\"\" class=\"words\"></span>-made."
+}
