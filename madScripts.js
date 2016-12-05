8e6d66336dde8f3b79f1d914d214ef2aeeb72ecf
archer77 <corleyx@yahoo.com>
2016-12-05 15:15:37 -0800

    add a case for bad entry into madLibs.html

--- madScripts.js
+++ madScripts.js
@@ -1,11 +1,20 @@
 //madScripts.js
 //Author:Alexander Corley
 
+var story;
+
 $(document).ready(function(){
   $(".nav > ul > ul > li").click(function(){
     setCookie("madLib", this.innerHTML, .5);
     window.location = "madLibs.html";
   });
+  if (window.location == "madLibs.html") {
+    story = madLibs[getCookie("madLib")];
+    var inputBox = $("#getWords");
+    if (story == undefined) {
+      inputBox.innerHTML = "Whoa how did you get here without a story? Click a story on the side to load a new story";
+    }
+  }
 });
 
 /**
@@ -14,7 +23,7 @@ $(document).ready(function(){
  */
 function setCookie(cname, cvalue, exdays) {
   var d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  d.setTime(d.getTime() + (exdays*60*60*1000));
   var expires = "expires="+ d.toUTCString();
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
