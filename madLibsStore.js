5af31a8d264c67723b1e820ba90bf5432e90dd00
archer77 <corleyx@yahoo.com>
2016-12-05 15:14:44 -0800

    change format for story storage
    and add a new story "Letter From Home"

--- madLibsStore.js
+++ madLibsStore.js
@@ -2,7 +2,7 @@
 //Author:Alexander Corley
 
 var madLibs = {
-  PizzaParlor : {
+  "Pizza Parlor" : {
     
     words : [
       "Name",
@@ -16,7 +16,32 @@ var madLibs = {
       "Noun"
     ],
     
-    Story : "Come on over to <span id=\"\" class=\"words\"></span>`s Pizza Parlor where you can enjoy you favorite <span id=\"\" class=\"words\"></span>-dish pizza`s. You can try our famous <span id=\"\" class=\"words\"></span>-lovers pizza, or select from our list of <span id=\"\" class=\"words\"></span> toppings, including sauteed <span id=\"\" class=\"words\"></span>, <span id=\"\" class=\"words\"></span>, and many more. Our crusts are hand-<span id=\"\" class=\"words\"></span> and basted in <span id=\"\" class=\"words\"></span> to make them seem more <span id=\"\" class=\"words\"></span>-made."
+    Story : "Come on over to <span class=\"words\"></span>`s Pizza Parlor where you can enjoy you favorite <span class=\"words\"></span>-dish pizza`s. You can try our famous <span class=\"words\"></span>-lovers pizza, or select from our list of <span class=\"words\"></span> toppings, including sauteed <span class=\"words\"></span>, <span class=\"words\"></span>, and many more. Our crusts are hand-<span class=\"words\"></span> and basted in <span class=\"words\"></span> to make them seem more <span class=\"words\"></span>-made."
+  },
+  
+  "Letter From Camp" : {
+    
+    words : [
+      "Relative",
+      "Adjective",
+      "Adjective",
+      "Adjective",
+      "Name of Person In Room",
+      "Adjective",
+      "Adjective",
+      "Verb Ending in \"ed\"",
+      "Body Part",
+      "Verb Ending in \"ing\"",
+      "Noun (Plural)",
+      "Noun",
+      "Adverb",
+      "Verb",
+      "Verb",
+      "Relative",
+      "Person in Room"
+    ],
+    
+    Story : "Dear <span class=\"words\"></span>, I am having a(n) <span class=\"words\"></span> time at camp. The counselour is <span class=\"words\"></span> and the food is <span class=\"words\"></span>. I met <span class=\"words\"></span> and we became <span class=\"words\"></span> friends. Unfortunately, <span class=\"words\"></span> is <span class=\"words\"></span> and I <span class=\"words\"></span> my <span class=\"words\"></span> so we couldn\`t go <span class=\"words\"></span> like everybody else. I need more <span class=\"words\"></span> and a <span class=\"words\"></span> sharpener, so please <span class=\"words\"></span> <span class=\"words\"></span> more when you <span class=\"words\"></span> back. Your <span class=\"words\"></span>, <span class=\"words\"></span>"
   }
 
 }
